7b4032c2f17f90df2e2984578ae55dcff9cb5e61
Rigel-Sile <jsilva@uni.pe>
2017-05-23 17:23:44 -0500

    xml layouts changed

---
+++ app/src/main/java/pe/ggarridomuni/tucuteam/fragment/ContactosFragment.java
@@ -0,0 +1,40 @@
+package pe.ggarridomuni.tucuteam.fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.Transaction;
+import pe.ggarridomuni.tucuteam.PostDetailActivity;
+import pe.ggarridomuni.tucuteam.R;
+import pe.ggarridomuni.tucuteam.models.Post;
+import pe.ggarridomuni.tucuteam.Views.PostViews;
+/**
+ * Created by shinji on 5/22/17.
+ */
+
+public class ContactosFragment extends Fragment {
+    public ContactosFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contactos, container, false);
+    }
+}
