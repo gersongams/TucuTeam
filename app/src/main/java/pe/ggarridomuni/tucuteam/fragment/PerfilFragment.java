6a6d284095d1269885057cb244128582c6f8ca79
geckolml <rcerront@uni.pe>
2017-06-14 18:33:20 -0500

    Agregando Perfil ....

---
+++ app/src/main/java/pe/ggarridomuni/tucuteam/fragment/PerfilFragment.java
@@ -0,0 +1,25 @@
+package pe.ggarridomuni.tucuteam.fragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pe.ggarridomuni.tucuteam.R;
+
+/**
+ * Created by shinji on 5/22/17.
+ */
+
+public class PerfilFragment extends Fragment {
+    public PerfilFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_perfil, container, false);
+    }
+}
